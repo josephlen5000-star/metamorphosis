813c891a5a012f77cd03a24097dc4ceb0d311882
Joseph Le Nevez <josephlen5000@gmail.com>
2026-09-05 23:40:26 -0700

    Remove unused mobile Firebase config

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart';
 
 class DefaultFirebaseOptions {
   static const FirebaseOptions web = FirebaseOptions(
@@ -11,56 +10,5 @@ class DefaultFirebaseOptions {
     projectId: 'metamorphosis-66fd1',
   );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCGR4iRDiZOmoNhMD_Zsgheg87fR3H_Lz8',
-    appId: '1:5863588128:android:605fbe0341c0aec00b1745',
-    messagingSenderId: '5863588128',
-    projectId: 'metamorphosis-66fd1',
-  );
-
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyA41XGMzb1jlRcLn0LV5Boi-abZ2Pt8PdA',
-    appId: '1:5863588128:ios:c839d377080796170b1745',
-    messagingSenderId: '5863588128',
-    projectId: 'metamorphosis-66fd1',
-  );
-
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'YOUR_MACOS_API_KEY',
-    appId: 'YOUR_MACOS_APP_ID',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-  );
-
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'YOUR_WINDOWS_API_KEY',
-    appId: 'YOUR_WINDOWS_APP_ID',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-  );
-
-  static const FirebaseOptions linux = FirebaseOptions(
-    apiKey: 'YOUR_LINUX_API_KEY',
-    appId: 'YOUR_LINUX_APP_ID',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-  );
-
-  static FirebaseOptions get currentPlatform {
-    // ignore: deprecated_member_use
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return android;
-      case TargetPlatform.iOS:
-        return ios;
-      case TargetPlatform.macOS:
-        return macos;
-      case TargetPlatform.windows:
-        return windows;
-      case TargetPlatform.linux:
-        return linux;
-      default:
-        return web;
-    }
-  }
+  static FirebaseOptions get currentPlatform => web;
 }
