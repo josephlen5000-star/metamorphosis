a9e16e7d54eb4e3aba61789fc9d03047fb22aa27
Joseph Le Nevez <josephlen5000@gmail.com>
2026-09-05 22:09:21 -0700

    Set up Firebase hosting

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -8,8 +7,10 @@ import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: kIsWeb ? DefaultFirebaseOptions.web : null,
   );
-  runApp(DevicePreview(enabled: true, builder: (context) => const MainApp()));
+
+  runApp(const MainApp());
 }
